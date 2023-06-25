a8b787ff2dd050bb15adc1fc78aa60379a3c659e
Léon Delcroix <leondecheux@gmail.com>
2023-06-25 18:28:49 +0200

    cleaning template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,15 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
 
 	<main>
 		<slot />
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>Isn't this better ?</p>
 	</footer>
 </div>
 
